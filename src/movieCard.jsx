dd9fa07d3e3da5e987118d982de91c7b0387963d
tekra-666 <shivinboxmail@gmail.com>
2022-10-30 18:00:21 +0530

    modified:   src/movieCard.jsx

--- src/movieCard.jsx
+++ src/movieCard.jsx
@@ -7,7 +7,7 @@ const movieCard = ({movie}) => {
         <p>{movie.Year}</p>
       </div>
       <div>
-      <img src={movie.Poster !== 'NA' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
+      <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title} />
       </div>
       <div>
       <span>{movie.Type}</span>
